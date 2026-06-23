2f3b13817baf2293ffa0abfa4413f83bb9ce3615
Sadiqinuwa <sadiqinuwa6@gmail.com>
2026-06-23 15:10:32 +0400

    Enhance metabolic syndrome detection interface
    Added progress indicator for detection process and updated interface text.

--- metabolic_syn_detection.py
+++ metabolic_syn_detection.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import joblib
 import streamlit as st
+import time
 
 #load model and encoder
 model = joblib.load("metabolic_syn_model.pkl")
@@ -9,28 +10,28 @@ encoder = joblib.load("metabolic_syn_encoder.pkl")
 
 #creating streamlit interface
 name = "Sadiq Inuwa"
-goal = "1.To predicts a particular class of anemia from the provided clinical values. 2.To confirm the legitimates of a test result !"
-st.sidebar.title("ABOUT US!")
-st.sidebar.caption(f"Developer: {name}.")
-st.sidebar.title("CONTACTS US...")
+goal = "To detects metabolic syndrome status (Yes or No) "
+st.sidebar.title("Developer")
+st.sidebar.caption(name)
+st.sidebar.title("CONTACTS")
 st.sidebar.link_button("facebook", 'https://www.facebook.com/sadeeq.alhinuwa')
 st.sidebar.link_button("email", 'sadiqinuwa6@gmail.com')
 st.sidebar.link_button("WhatsApp", 'https://wa.me/2347067717477')
-st.title("ANEMIA CLASS PREDICTION")
+st.title("METABOLIC SYNDROME DETECTION")
 st.caption(f"GOAL: {goal}")
 
 
 #accepting users input
-Age = st.number_input("Enter patient's Age Value!")
-Sex = st.selectbox("Select patient's sex!", [ "Male", "Female" ])
-Marital = st.selectbox("Select patient's marital status!", ["Married","Single","Widowed","Separated"])
-Race = st.selectbox("Select patient's race!", ["White","Black","Asian","Hispanic","MexAmerican"])
-BMI = st.number_input("Enter patient's BMI(Body Mass Index) Value!")
-UrAlbCr = st.number_input("Enter patient's UrAlbCr value!")
-UricAcid = st.number_input("Enter patient's UricAcid value!")
-BloodGlucose = st.number_input("Enter patient's BloodGlucose value!")
-HDL =st.number_input("Enter patient's HDL value!")
-Triglycerides = st.number_input("Enter patient's Triglycerides value!")
+Age = st.number_input("Enter patient's Age Value:")
+Sex = st.selectbox("Select patient's gender:", [ "Male", "Female" ])
+Marital = st.selectbox("Select patient's marital status:", ["Married","Single","Widowed","Separated"])
+Race = st.selectbox("Select patient's race:", ["White","Black","Asian","Hispanic","MexAmerican"])
+BMI = st.number_input("Enter patient's BMI(Body Mass Index) Value:")
+UrAlbCr = st.number_input("Enter patient's UrAlbCr level:")
+UricAcid = st.number_input("Enter patient's UricAcid level:")
+BloodGlucose = st.number_input("Enter patient's Blood Glucose level:")
+HDL =st.number_input("Enter patient's HDL level:")
+Triglycerides = st.number_input("Enter patient's Triglycerides level:")
 
 
 #prediction dataframe
@@ -49,11 +50,19 @@ if st.button("Detect Metabolic Syndrome"):
    })
 
     #transform the predicted dataframe
+    progress = st.progress(0, text="Detecting Metabolic Syndrome...")
     predicted_data = encoder.transform(prediction_data)
     prediction = model.predict(predicted_data)
-    if prediction == 1:
-        st.success("Yes!")
-    elif prediction == 0:
-        st.warning("No!")
+
+    for i in range(100):
+        time.sleep(0.02)
+        progress.progress(i + 1, "Detecting Metabolic Syndrome...")
+    progress.empty()
+    if prediction[0] == 1:
+        st.badge("YES! - Metabolic Syndrome Detected", color="red")
+    elif prediction[0] == 0:
+        st.badge("NO! - Metabolic Syndrome Not Detected", color="green")
+
+
     else:
         st.warning("Please enter the correct inputs!")
